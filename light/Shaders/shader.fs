36e3221ea5e39ba9d3c3e687feb8eb147d6595ad
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-07-18 19:52:18 +0700

    diffuse Map

--- light/Shaders/shader.fs
+++ light/Shaders/shader.fs
@@ -1,34 +1,46 @@
 #version 330 core
+
+struct Material {
+    sampler2D   diffuse;  
+    vec3        specular;
+    float       shininess;
+};
+
+struct Light {
+    vec3 position;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;  
+uniform Material material;
+
 out vec4 FragColor;
 
+in vec2 TexCoords;
 in vec3 FragPos;  
 in vec3 Normal;
   
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;  
 uniform vec3 viewPos;
 
 void main()
 {
-    float ambientStrength = 0.1f;
-    float specularStrength = 0.5f;
-    int shininess = 32;
-
-    vec3 ambient = ambientStrength * lightColor;
-
+    //diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);  
-
+    vec3 lightDir = normalize(light.position - FragPos);  
     float diff = max(dot(norm, lightDir), 0.0f);
-    vec3 diffuse = diff * lightColor;
 
+    //specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), shininess);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 ambient  = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse  = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * (spec * material.specular);  
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+    vec3 result = ambient + diffuse + specular; //not multiply objColor because seperate through diffent kind of lighting
+    FragColor = vec4(result, 1.0f);
 }
